5544e835ae053539a5ca4013fd09fc3001153893
Ryan Blecher <notryanb@gmail.com>
2024-10-15 21:17:00 -0400

    Nice! The filter actually works. I don't like using a Mutex in audio :(

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,31 @@
+use std::sync::mpsc::Sender;
 
+pub enum AudioCommand {
+    SetVolume(f32),
+    SetFilterFreq(f32),
+    SetResonance(f32),
+}
 
 pub struct AudioFilterApp {
+    pub vol: f32,
+    pub freq_hz: f32,
+    pub resonance_q: f32,
+    pub audio_tx: Option<Sender<crate::app::AudioCommand>>,
 }
 
 impl Default for AudioFilterApp {
     fn default() -> Self {
-        Self {}
+        Self {
+            vol: 0.3,
+            freq_hz: 1000.0,
+            resonance_q: 0.707,
+            audio_tx: None,
+        }
     }
 }
 
 impl AudioFilterApp {
-    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new() -> Self {
         Default::default()
     }
 }
@@ -23,6 +38,30 @@ impl eframe::App for AudioFilterApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::TopBottomPanel::top("Top Panel").show(ctx, |ui| {
             ui.heading("Audio Filter App");
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let volume_slider = ui.add(egui::Slider::new(&mut self.vol, 0.0..=1.0).text("Volume").logarithmic(true));
+            let freq_slider = ui.add(egui::Slider::new(&mut self.freq_hz, 20.0..=18000.0).text("Frequency Hz").logarithmic(true));
+            let reso_slider = ui.add(egui::Slider::new(&mut self.resonance_q, 0.707..=50.0).text("Resonance (Q)").logarithmic(true));
+
+            if volume_slider.dragged() {
+                if let Some(tx) = &self.audio_tx {
+                    _ = tx.send(AudioCommand::SetVolume(self.vol));
+                }
+            }
+
+            if freq_slider.dragged() {
+                if let Some(tx) = &self.audio_tx {
+                    _ = tx.send(AudioCommand::SetFilterFreq(self.freq_hz));
+                }
+            }
+
+            if reso_slider.dragged() {
+                if let Some(tx) = &self.audio_tx {
+                    _ = tx.send(AudioCommand::SetResonance(self.resonance_q));
+                }
+            }
         });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@
                                                                    // Windows in release mode
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::sync::mpsc::channel;
+use atomic_float::AtomicF32;
+use std::sync::atomic::Ordering;
+use std::sync::{Arc, Mutex};
+
+mod app;
+
+use crate::app::{AudioFilterApp, AudioCommand};
 
 pub struct StateVariableFilter {
     pub sample_rate: f32,
@@ -82,52 +90,98 @@ impl NoiseGen {
 fn main() -> eframe::Result {
     env_logger::init();
 
+    let svf = Arc::new(Mutex::new(StateVariableFilter::new(44100.0)));
+    {
+        let mut f = svf.lock().unwrap();
+        f.reset();
+    }
+
+    // I don't want to store everything in a mutex, so at least for now keep the volume as an Atomic and pass the filter behind
+    // an Arc<Mutex>
+    let volume = std::sync::Arc::new(AtomicF32::new(0.3));
+    let mut cutoff_freq_hz = 1000.0;
+    let mut resonance_q = 0.707;
+    let (ui_tx, ui_rx) = channel::<crate::app::AudioCommand>();
+
     let _audio_thread = std::thread::spawn(move || {
+        let volume_clone = volume.clone();
+        let filter = svf.clone();
+
         let host = cpal::default_host();
 
         let device = host.default_output_device().expect("Failed to get default output device");
         let config = device.default_output_config().expect("Failed to get default device config");
 
-        match config.sample_format() {
-            cpal::SampleFormat::I8 =>  make_stream::<i8>(&device, &config.into()),
-            cpal::SampleFormat::I16 => make_stream::<i16>(&device, &config.into()),
-            cpal::SampleFormat::I32 => make_stream::<i32>(&device, &config.into()),
-            cpal::SampleFormat::I64 => make_stream::<i64>(&device, &config.into()),
-            cpal::SampleFormat::U8 =>  make_stream::<u8>(&device, &config.into()),
-            cpal::SampleFormat::U16 => make_stream::<u16>(&device, &config.into()),
-            cpal::SampleFormat::U32 => make_stream::<u32>(&device, &config.into()),
-            cpal::SampleFormat::U64 => make_stream::<u64>(&device, &config.into()),
-            cpal::SampleFormat::F32 => make_stream::<f32>(&device, &config.into()),
-            cpal::SampleFormat::F64 => make_stream::<f64>(&device, &config.into()),
+        let stream = match config.sample_format() {
+            cpal::SampleFormat::I8 =>  make_stream::<i8>(&device, &config.into(),  volume_clone, filter),
+            cpal::SampleFormat::I16 => make_stream::<i16>(&device, &config.into(), volume_clone, filter),
+            cpal::SampleFormat::I32 => make_stream::<i32>(&device, &config.into(), volume_clone, filter),
+            cpal::SampleFormat::I64 => make_stream::<i64>(&device, &config.into(), volume_clone, filter),
+            cpal::SampleFormat::U8 =>  make_stream::<u8>(&device, &config.into(),  volume_clone, filter),
+            cpal::SampleFormat::U16 => make_stream::<u16>(&device, &config.into(), volume_clone, filter),
+            cpal::SampleFormat::U32 => make_stream::<u32>(&device, &config.into(), volume_clone, filter),
+            cpal::SampleFormat::U64 => make_stream::<u64>(&device, &config.into(), volume_clone, filter),
+            cpal::SampleFormat::F32 => make_stream::<f32>(&device, &config.into(), volume_clone, filter),
+            cpal::SampleFormat::F64 => make_stream::<f64>(&device, &config.into(), volume_clone, filter),
             _sample_format => panic!("Unexpected sample format!!!"),
+        };
+
+        // If the stream is disposed of, audio is DONE. The stream needs to be alive for the duration of the app.
+        // This is okay because the thread is looping forever to try and read commands from the UI, so it'll never stop.
+        // I wonder if the stream could instead be stored in the App's state...?
+        stream.play().unwrap();
+
+        // make updates
+        loop {
+            match ui_rx.try_recv() {
+                Ok(cmd) => {
+                    match cmd {
+                        AudioCommand::SetVolume(new_vol) => { 
+                            volume.store(new_vol, Ordering::Relaxed);
+                        }
+                        AudioCommand::SetFilterFreq(cuttoff_freq_hz_new) => { 
+                            cutoff_freq_hz = cuttoff_freq_hz_new;
+                            {
+                                let mut filter = svf.lock().unwrap();
+                                filter.update_coefficients(cutoff_freq_hz, resonance_q);                                
+                            }
+                        }
+                        AudioCommand::SetResonance(resonance_q_new)  => {
+                            resonance_q = resonance_q_new;
+                            {
+                                let mut filter = svf.lock().unwrap();
+                                filter.update_coefficients(cutoff_freq_hz, resonance_q); 
+                            }
+                        }
+                    }
+                }
+                Err(_) => (),
+            }
         }
-    });
+    }); // Audio Thread End
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
+            .with_inner_size([800.0, 600.0])
             .with_min_inner_size([300.0, 220.0]),
         ..Default::default()
     };
 
+    let mut app = AudioFilterApp::new();
+    app.audio_tx = Some(ui_tx);
+
     eframe::run_native(
         "Audio Filters",
         native_options,
-        Box::new(|cc| Ok(Box::new(filters::AudioFilterApp::new(cc)))),
+        Box::new(|_| Ok(Box::new(app))),
     )
 }
 
-fn make_stream<T>(device: &cpal::Device, config: &cpal::StreamConfig) 
+fn make_stream<T>(device: &cpal::Device, config: &cpal::StreamConfig, volume: Arc<AtomicF32>, filter: Arc<Mutex<StateVariableFilter>>) -> cpal::Stream
     where T: cpal::SizedSample + cpal::FromSample<f32> 
 {
     let num_channels = config.channels as usize;
     let mut noise_gen = NoiseGen::new();
-    let mut svf = StateVariableFilter::new(44100.0);
-    svf.reset();
-
-    let cuttoff_freq_hz= 440.0;
-    let resonance_q = 0.707;
-    svf.update_coefficients(cuttoff_freq_hz, resonance_q);
 
     let err_fn = |err| eprintln!("Error building output sound stream {}", err);
 
@@ -135,22 +189,39 @@ fn make_stream<T>(device: &cpal::Device, config: &cpal::StreamConfig)
         .build_output_stream(
             config,
             move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
-                process_frame(output, &mut noise_gen, &mut svf, num_channels)
+                // process_frame(output, &mut noise_gen, &mut svf, vol, num_channels)
+                process_frame(output, &mut noise_gen, num_channels, volume.clone(), filter.clone())
             },
             err_fn,
             None,
         )
         .expect("Failed to build output stream...");
 
-    stream.play().unwrap();
-    std::thread::sleep(std::time::Duration::from_secs(30));
+    stream
 }
 
-fn process_frame<SampleType>(output: &mut [SampleType], noise_gen: &mut NoiseGen, filter: &mut StateVariableFilter, num_channels: usize)
+fn process_frame<SampleType>(
+    output: &mut [SampleType], 
+    noise_gen: &mut NoiseGen, 
+    num_channels: usize,
+    volume: Arc<AtomicF32>,
+    filter: Arc<Mutex<StateVariableFilter>>
+)
     where SampleType: cpal::Sample + cpal::FromSample<f32> 
 {
+    let volume = volume.load(Ordering::Relaxed);
+
     for frame in output.chunks_mut(num_channels) {
-        let next_sample = filter.render(noise_gen.next_value());
+        let next_noise = noise_gen.next_value();
+
+        // TODO: Not great...  I don't like locks in the audio thread as they're unbounded.
+        // Lock for the shortest span possible... just to render the filter's output.
+        let filter_sample: f32;
+        {
+            let mut svf = filter.lock().unwrap();
+            filter_sample = svf.render(next_noise);
+        }
+        let next_sample = filter_sample * volume;
         let value: SampleType = SampleType::from_sample(next_sample);
 
         for sample in frame.iter_mut() {
