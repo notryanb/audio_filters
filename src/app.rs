77213f09adb2c9039afa8bbbcb6ed2a11b946f20
Ryan Blecher <notryanb@gmail.com>
2024-11-25 21:18:11 -0500

    display the FFT of the filter's impulse response, but it is totally arbitrary right now and doesn't interact with the UI

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,9 @@
 use std::sync::mpsc::Sender;
+use egui::epaint::*;
+use egui::{pos2, vec2, Frame, Pos2, Rect};
+use egui_plot::{Line, Plot, PlotPoints};
+
+use crate::StateVariableFilter;
 
 pub enum AudioCommand {
     SetVolume(f32),
@@ -11,6 +16,7 @@ pub struct AudioFilterApp {
     pub freq_hz: f32,
     pub resonance_q: f32,
     pub audio_tx: Option<Sender<crate::app::AudioCommand>>,
+    pub filter_freq_res: Option<Vec<f32>>,
 }
 
 impl Default for AudioFilterApp {
@@ -20,8 +26,14 @@ impl Default for AudioFilterApp {
             freq_hz: 1000.0,
             resonance_q: 0.707,
             audio_tx: None,
+            filter_freq_res: None,
         }
     }
+}
+
+pub trait AppComponent {
+    type Context;
+    fn add(ctx: &mut Self::Context, ui: &mut eframe::egui::Ui);
 }
 
 impl AudioFilterApp {
@@ -36,6 +48,32 @@ impl eframe::App for AudioFilterApp {
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if *(&self.filter_freq_res.is_none()) {
+            use realfft::RealFftPlanner;
+            use realfft::num_complex::ComplexFloat;
+
+            let sample_rate = 44100;
+
+            let mut svf = StateVariableFilter::new(sample_rate as f32);
+            svf.update_coefficients(1000.0, 0.707);
+            let mut impulse: Vec<f32> = (0..sample_rate).map(|_| 0.0).collect();
+            impulse[0] = 1.0;
+
+            let mut impulse_response = impulse.iter().map(|sample| svf.render(*sample)).collect::<Vec<f32>>();
+
+            let mut real_planner = RealFftPlanner::<f32>::new();
+            let r2c = real_planner.plan_fft_forward(sample_rate);
+            let mut spectrum = r2c.make_output_vec();
+            r2c.process(&mut impulse_response[..], &mut spectrum).expect("failed to process FFT");
+            
+            // instead of graphing the impulse response, calculate the FFT of the impulse response.
+            // Ensure the 20 * log10(spectrum_bin.abs()) is used to plot the absolute value of the
+            // spectrum bin converted to dB
+            self.filter_freq_res = Some(spectrum.iter().map(|f| (f.re.abs() + std::f32::EPSILON).log10() * 20.0).collect());
+            //self.filter_freq_res = Some(impulse_response);
+        }
+
+        
         egui::TopBottomPanel::top("Top Panel").show(ctx, |ui| {
             ui.heading("Audio Filter App");
         });
@@ -61,6 +99,15 @@ impl eframe::App for AudioFilterApp {
                 if let Some(tx) = &self.audio_tx {
                     _ = tx.send(AudioCommand::SetResonance(self.resonance_q));
                 }
+            }
+
+            // for 0 to half-nyquist, plot frequency response
+            if let Some(filter_freq_res) = &self.filter_freq_res {
+                let sin: PlotPoints = filter_freq_res.iter().enumerate().map(|(x, y)| {
+                    [x as f64, *y as f64]
+                }).collect();
+                let line = Line::new(sin);
+                Plot::new("frequencies").view_aspect(2.0).show(ui, |plot_ui| plot_ui.line(line));
             }
         });
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,61 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
+
 pub use app::AudioFilterApp;
+
+pub struct StateVariableFilter {
+    pub sample_rate: f32,
+    g: f32, // cutoff freq
+    k: f32, // resonance
+    a1: f32,
+    a2: f32,
+    a3: f32,
+    ic1eq: f32,
+    ic2eq: f32,
+}
+
+impl StateVariableFilter {
+    pub fn new(sample_rate: f32) -> Self {
+        Self {
+            sample_rate,
+            g: 0.0,
+            k: 0.0,
+            a1: 0.0,
+            a2: 0.0,
+            a3: 0.0,
+            ic1eq: 0.0,
+            ic2eq: 0.0,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.g = 0.0;
+        self.k = 0.0;
+        self.a1 = 0.0;
+        self.a2 = 0.0;
+        self.a3 = 0.0;
+        self.ic1eq = 0.0;
+        self.ic2eq = 0.0;
+    }
+
+    pub fn update_coefficients(&mut self, cutoff: f32, q: f32) {
+        self.g = (std::f32::consts::PI * cutoff / self.sample_rate).tan();
+        self.k = 1.0 / q;
+        self.a1 = 1.0 / (1.0 + self.g * (self.g + self.k));
+        self.a2 = self.g * self.a1;
+        self.a3 = self.g * self.a2;
+    }
+
+    pub fn render(&mut self, sample: f32) -> f32 {
+        // v1..v3 are voltages at different nodes
+        let v3 = sample - self.ic2eq;
+        let v1 = self.a1 * self.ic1eq + self.a2 * v3;
+        let v2 = self.ic2eq + self.a2 * self.ic1eq + self.a3 * v3; // lp
+        self.ic1eq = 2.0 * v1 - self.ic1eq; // state of capacitors
+        self.ic2eq = 2.0 * v2 - self.ic2eq; // state of capacitors
+        v2
+    }
+}
+
+
